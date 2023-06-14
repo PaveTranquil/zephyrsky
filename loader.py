d533cc255731d8e62abcccfe77049f71b5c7694b
Pavel Ovchinnikov <opavel@internet.ru>
2023-06-15 02:25:38 +0300

    feat(loader): реализована инициализация необходимых объектов
    - `get` и `set_` работают с переменными окружения в `.env`
    - `bot` — основная прокладка между кодом и чат-ботом
    - `storage` — хранилище состояний (синхронизируется с `User.state` в случае краша)
    - `dp` —  диспетчер, управляет хендлерами и распределяет по ним события
    - `db` — экземпляр созданной мною базы данных
    - `scheduler` — планировщик событий для отправки уведомлений с погодой
    - `ADMINS` — список админов, выгруженный из переменных окружения

---
+++ loader.py
@@ -0,0 +1,24 @@
+import sys
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from dotenv import dotenv_values, set_key
+
+from database import Database
+
+get = lambda key: dotenv_values(".env").get(key)
+set_ = lambda key, value: set_key(".env", key, value.encode("utf-8").decode("windows-1251"))
+
+
+bot = Bot(get("BOT_TOKEN"), parse_mode="HTML")
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+db = Database(get("DATABASE_URL"))
+scheduler = AsyncIOScheduler()
+try:
+    ADMINS = [int(admin) for admin in get("ADMINS").replace(", ", ",").split(",")]
+except (AttributeError, ValueError):
+    print("Добавьте в .env список админов через запятую")
+    sys.exit(1)
